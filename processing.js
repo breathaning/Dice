763ba3657e58c726bc3e78419b4ae602d8181680
breathaning <etzhu1@gmail.com>
2025-09-25 20:38:24 -0700

    fix length

--- processing.js
+++ processing.js
@@ -13856,7 +13856,7 @@ module.exports = function setupParser(Processing, options) {
           obj = ctx.createImageData(w, h),
           uBuff = new Uint8Array(w * h * 4);
       curContext.readPixels(x, y, w, h, curContext.RGBA, curContext.UNSIGNED_BYTE, uBuff);
-	  var dataArray = [];
+	  var dataArray = new Array(uBuff.length);
 	  for (var i=0, ul=uBuff.length; i < ul; i += w * 4) {
 		  dataArray.push(...uBuff.slice(ul - w * 4 * (i - 1), ul - w * 4 * i));
       }
