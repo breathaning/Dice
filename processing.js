2eed86dcd1f94a56ba0e1836c1b9b109fa160680
breathaning <etzhu1@gmail.com>
2025-09-25 21:40:42 -0700

    Update processing.js

--- processing.js
+++ processing.js
@@ -13861,7 +13861,7 @@ module.exports = function setupParser(Processing, options) {
 	  for (var i=0; i < h; i++) {
         reversedBuffer.push(uBuff.slice(i *  increment, (i + 1) * increment).reverse());
       }
-		console.log(uBuff);
+		console.log(reversedBuffer);
 	  obj.data.set(reversedBuffer);
       return obj;
     };
