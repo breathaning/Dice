f464b4a2ddd66d7e0f30f57a66667c5f318ba3e7
breathaning <etzhu1@gmail.com>
2025-09-26 10:53:33 -0700

    fix indent (serious)

--- processing.js
+++ processing.js
@@ -7038,7 +7038,7 @@
       if (p.imageData) drawing.$ensureContext().putImageData(p.imageData, 0, 0)
     };
     p.hint = function(which) {
-       var curContext = drawing.$ensureContext();
+      var curContext = drawing.$ensureContext();
       try {
       if (which === 4) {
         curContext.disable(curContext.DEPTH_TEST);
